d529bd2e26d9ccdd8a5ea17873cf9e11b13225b1
BryanA <bryan.ayala02@epn.edu.ec>
2026-05-25 10:50:11 -0500

    Implementación de la capa de indicadores: ATR (Wilder incremental) e IndicatorManager

--- Market/IndicatorManager.pm
+++ Market/IndicatorManager.pm
@@ -3,31 +3,63 @@ use strict;
 use warnings;
 
 sub new {
-my ($class) = @_;
-my $self = {
-indicators => {},
-};
-bless $self, $class;
-return $self;
+    my ($class) = @_;
+    my $self = {
+        indicators => {},
+    };
+    bless $self, $class;
+    return $self;
 }
+
 sub register {
-my ($self, $name, $indicator) = @_;
-# TODO
+    my ($self, $name, $indicator) = @_;
+    die "register: name is required"      unless defined $name && length $name;
+    die "register: indicator is required" unless defined $indicator;
+    die "register: indicator '$name' does not implement update_last"
+        unless $indicator->can('update_last');
+    die "register: indicator '$name' does not implement get_values"
+        unless $indicator->can('get_values');
+
+    $self->{indicators}->{$name} = $indicator;
+    return $self;
 }
+
 sub update_last {
-my ($self, $market_data) = @_;
-# TODO
+    my ($self, $market_data) = @_;
+    return unless defined $market_data;
+
+    for my $name (keys %{ $self->{indicators} }) {
+        $self->{indicators}->{$name}->update_last($market_data);
+    }
+    return;
 }
+
 sub get {
-my ($self, $name) = @_;
-# TODO
+    my ($self, $name) = @_;
+    my $indicator = $self->{indicators}->{$name};
+    return undef unless $indicator;
+    return $indicator->get_values();
 }
+
 sub slice_array {
-my ($self, $name, $start, $end) = @_;
-# TODO
+    my ($self, $name, $start, $end) = @_;
+    my $values = $self->get($name);
+    return [] unless $values && @$values;
+
+    $start = 0         if !defined $start || $start < 0;
+    $end   = $#$values if !defined $end   || $end > $#$values;
+    return [] if $start > $end;
+
+    return [ @{$values}[$start .. $end] ];
 }
+
 sub reset_all {
-my ($self) = @_;
-# TODO
+    my ($self) = @_;
+    for my $name (keys %{ $self->{indicators} }) {
+        my $indicator = $self->{indicators}->{$name};
+        $indicator->reset() if $indicator->can('reset');
+    }
+    return;
 }
+
 1;
--- Market/Indicators/ATR.pm
+++ Market/Indicators/ATR.pm
@@ -3,24 +3,80 @@ use strict;
 use warnings;
 
 sub new {
-my ($class, $period) = @_;
-my $self = {
-period => $period,
-values => [],
-};
-bless $self, $class;
-return $self;
+    my ($class, $period) = @_;
+    die "ATR period must be a positive integer"
+        unless defined $period && $period =~ /^\d+$/ && $period > 0;
+
+    my $self = {
+        period      => $period,
+        values      => [],
+        _tr_sum     => 0,
+        _last_close => undef,
+        _last_atr   => undef,
+        _count      => 0,
+    };
+    bless $self, $class;
+    return $self;
 }
+
 sub update_last {
-my ($self, $market_data) = @_;
-# TODO
+    my ($self, $market_data) = @_;
+    my $candle = $market_data->last_candle();
+    return unless $candle;
+
+    my $high  = $candle->[2];
+    my $low   = $candle->[3];
+    my $close = $candle->[4];
+
+    my $tr;
+    if (defined $self->{_last_close}) {
+        my $prev_close = $self->{_last_close};
+        my $hl  = $high - $low;
+        my $hpc = abs($high - $prev_close);
+        my $lpc = abs($low  - $prev_close);
+        $tr = $hl;
+        $tr = $hpc if $hpc > $tr;
+        $tr = $lpc if $lpc > $tr;
+    } else {
+        $tr = $high - $low;
+    }
+
+    $self->{_count}++;
+    my $period = $self->{period};
+
+    if ($self->{_count} < $period) {
+        $self->{_tr_sum} += $tr;
+        push @{ $self->{values} }, undef;
+    }
+    elsif ($self->{_count} == $period) {
+        $self->{_tr_sum} += $tr;
+        my $atr = $self->{_tr_sum} / $period;
+        $self->{_last_atr} = $atr;
+        push @{ $self->{values} }, $atr;
+    }
+    else {
+        my $atr = ($self->{_last_atr} * ($period - 1) + $tr) / $period;
+        $self->{_last_atr} = $atr;
+        push @{ $self->{values} }, $atr;
+    }
+
+    $self->{_last_close} = $close;
+    return;
 }
+
 sub get_values {
-my ($self) = @_;
-# TODO
+    my ($self) = @_;
+    return $self->{values};
 }
+
 sub reset {
-my ($self) = @_;
-# TODO
+    my ($self) = @_;
+    $self->{values}      = [];
+    $self->{_tr_sum}     = 0;
+    $self->{_last_close} = undef;
+    $self->{_last_atr}   = undef;
+    $self->{_count}      = 0;
+    return;
 }
+
 1;
